c7d53a03817ae5f41e9ce8e87ade6cbf01f8ebde
ErnestF22 <ernesto.fontana96@gmail.com>
2024-03-14 12:17:08 +0100

    Added POCRotateToMinimizeLastEntries

---
+++ genproc/POCRotateToMinimizeLastEntries.m
@@ -0,0 +1,19 @@
+function POCRotateToMinimizeLastEntries
+dLow=3;
+d=5;
+nbCols=10;
+
+% Generate low-rank data
+xLow=randn(dLow,nbCols);
+xTrue=[xLow; zeros(d-dLow,nbCols)];
+QTrue=rot_randn(eye(d));
+x=QTrue*xTrue;
+
+% Recover a Q for aligning the data
+[Q,~,~]=svd(x*x');
+disp('x=')
+disp(x)
+disp('Q''*x=')
+disp(Q'*x)
+
+
---
+++ genproc/solve_gauge_symmetry.m
@@ -0,0 +1,11 @@
+nrs = 4; %change to nrs = 5 to test it
+d = 3;
+N = 5;
+
+
+R_i = make_rand_stiefel_3d_array(nrs, d, N);
+T_i = 10 * rand(nrs, N);
+
+
+
+
