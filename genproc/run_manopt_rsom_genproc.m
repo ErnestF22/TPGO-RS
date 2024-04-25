8eb971b7402aba2d2d6595da76439cdf8890d84a
ErnestF22 <ernesto.fontana96@gmail.com>
2024-04-25 09:42:14 +0200

    Added Qbnn recovery test with test data

--- genproc/run_manopt_rsom_genproc.m
+++ genproc/run_manopt_rsom_genproc.m
@@ -21,8 +21,8 @@ riem_grad_mode = 'manual'; %'auto' or 'manual'
 hessian_mode = 'manual'; 
 initguess_is_available = boolean(0);
 rand_initguess = boolean(1);
-enable_manopt_icp = boolean(1);
-enable_procrustes = boolean(1);
+enable_manopt_icp = boolean(0);
+enable_procrustes = boolean(0);
 enable_manopt_rs = boolean(1);
 som_params = struct('N', N, 'd', d, 'd_aff', d_aff, ...
     'global_camera_id', global_camera_id, ...
@@ -42,8 +42,8 @@ som_params = struct('N', N, 'd', d, 'd_aff', d_aff, ...
 sigmas = readmatrix("../sigmas.txt"); %sigma = stdev, sigma.^2 = variance
 mus = readmatrix("../mus.txt"); %OBS. generally, mus can be d-dimensional; here, we just assume them as scalar (i.e. a d-dimensional vector with all coordinates equal)
 
-sigmas = sigmas(4);
-% sigmas = 0.0;
+% sigmas = sigmas(4);
+sigmas = 0.0;
 mus = mus(2);
 
 %If reading from file does not work and want to try a single noise_params
---
+++ genproc/test_Qbnn_recovery.m
@@ -0,0 +1,7 @@
+load('Qbnn_data/Qbnn_R.mat', 'R')
+load('Qbnn_data/Qbnn_T.mat', 'T')
+load('Qbnn_data/Qbnn_edges.mat', 'edges')
+load('Qbnn_data/Qbnn_Q.mat', 'Q')
+load('Qbnn_data/Qbnn_x_out.mat', 'x_out')
+load('Qbnn_data/Qbnn_T_diffs.mat', 'T_diffs')
+% load('Qbnn') %whole workspace
