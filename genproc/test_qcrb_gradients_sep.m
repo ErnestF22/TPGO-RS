a7ae7aa857f12a3bdbd327f9cfd997ec8eaed60e
ErnestF22 <ernesto.fontana96@gmail.com>
2024-06-04 10:59:42 +0200

    added test_qcrb_gradients_sep

--- genproc/test_qcrb_gradients_sep.m
+++ genproc/test_qcrb_gradients_sep.m
@@ -1,13 +1,4 @@
-function test_qcrb_gradients
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
-
-
-% load('Qbnn_data/Qbnn_R.mat', 'R')
-% load('Qbnn_data/Qbnn_T.mat', 'T')
-% load('Qbnn_data/Qbnn_edges.mat', 'edges')
-% load('Qbnn_data/x_rs.mat', 'x_rs')
-% load('Qbnn_data/Tijs.mat', 'Tijs')
+function test_qcrb_gradients_sep
 
 node_deg = 2;
 
@@ -26,20 +17,10 @@ disp("check_is_rotation(rb0)")
 disp(check_is_rotation(rb0))
 
 
-% [Rt,dRt,~,~,~,ddRt]=qcrb_geodFun(R0);
 [qcRt, dqcRt, ~, ~, ~, ~] = rot_geodFun(qc0, []);
 [rbRt, drbRt, ~, ~, ~, ~] = rot_geodFun(rb0, []);
 
 
-% i = 1;
-% j1 = 2;
-% j2 = 3;
-% R_i = R(:,:,1);
-% idx_i_j1 = find(ismember(edges, [i, j1], "rows"));
-% idx_i_j2 = find(ismember(edges, [i, j2], "rows"));
-% 
-% T_i_j1_j2 = [Tijs(:,idx_i_j1), Tijs(:,idx_i_j2)];
-% T_i_j1_j2_tilde = - [T(:,i) - T(:,j1), T(:,i) - T(:,j2)]; % !! -
 
 Qa = [-1.64302828263923e-16	0.106320705792361	-0.889261908456345	-0.444869830049638;
     0.993981726033715	-0.108890962053455	-0.0116398298358148	-0.00275700117918123;
@@ -56,22 +37,23 @@ Ri = [0.0864141652363848	0.0420429561776974	-0.993397284823312;
 
 % test qc
 figure(1)
-f=@(t) mycost_qc(qcRt(t), rb0, node_deg, Qa, Qcd_i, Ri);
-egradf=@(t) myegrad_qc(qcRt(t), rb0, node_deg, Qa, Qcd_i, Ri);
-df=@(t) stiefel_metric([],egradf(t),dqcRt(t));
-funCheckDer(f,df)
+f_qc=@(t) mycost_qc(qcRt(t), rb0, node_deg, Qa, Qcd_i, Ri);
+egradf_qc=@(t) myegrad_qc(qcRt(t), rb0, node_deg, Qa, Qcd_i, Ri);
+df_qc=@(t) stiefel_metric([],egradf_qc(t),dqcRt(t));
+funCheckDer(f_qc,df_qc)
 
 % test rb
 figure(2)
-f=@(t) mycost_rb(qc0, rbRt(t), node_deg, Qa, Qcd_i, Ri);
-egradf=@(t) myegrad_rb(qc0, rbRt(t), node_deg, Qa, Ri);
-df=@(t) stiefel_metric([],egradf(t),drbRt(t));
-funCheckDer(f,df)
+f_rb=@(t) mycost_rb(qc0, rbRt(t), node_deg, Qa, Qcd_i, Ri);
+egradf_rb=@(t) myegrad_rb(qc0, rbRt(t), node_deg, Qa, Ri);
+df_rb=@(t) stiefel_metric([],egradf_rb(t),drbRt(t));
+funCheckDer(f_rb,df_rb)
 
 
 
 end %file function
 
+%%
 function bool_is_rot = check_is_rotation(A)
     if (det(A) < 1-1e-6 || det(A) > 1+1e-6)
         disp("check_is_rotation() -> determinant = 1 check failed")
@@ -92,7 +74,7 @@ function bool_is_rot = check_is_rotation(A)
     bool_is_rot = boolean(1);
 end
 
-
+%%
 function c_out = mycost_qc(x, Rb, node_deg, Qa, Qcd_i, Ri)
 Qcdd_i = x;
 rb_i = Rb;
@@ -102,40 +84,29 @@ d = size(Ri,2);
 P = [zeros(p-d,d), eye(p-d)];
 A = P * Qcdd_i' * Qcd_i * Ri;
 % B
-%     Qb_i = eye(p);
-%     Qb_i(p-1:end, p-1:end) = rb_i;
 Qb_i = blkdiag(eye(p-node_deg), rb_i); %node_deg = 2
 B = Qcdd_i' - Qa' * Qb_i * Qa;
-
 % sum of norms
-c_out = norm(A(:))^2 + norm(B(:))^2;
+c_out = 0.5 * (norm(A(:))^2 + norm(B(:))^2);
 end
 
 
-function grad_out = myegrad_qc(x, Rb, node_deg, ...
-    Qa, Qcd_i, Ri)
+function grad_out = myegrad_qc(x, Rb, node_deg, Qa, Qcd_i, Ri)
 Qcdd_i = x;
 rb_i = Rb;
 p = size(Ri,1);
 d = size(Ri,2);
 % A
 P = [zeros(p-d,d), eye(p-d)];
-% Qcd_i = eye(p);
 A = P * Qcdd_i' * Qcd_i * Ri;
 % B
-%     Qb_i = eye(p);
-%     Qb_i(p-1:end, p-1:end) = rb_i;
 Qb_i = blkdiag(eye(p-node_deg), rb_i); %node_deg = 2
 B = Qcdd_i' - Qa' * Qb_i * Qa;
-% grad out (struct)
-% Qa1 = Qa(1:2, :);
-% Qa2 = Qa(3:end, :);
-grad_out = 2 * (Qcd_i * Ri * A' * P + B);
-%     grad_out.rb = - Q2 * B * Q2';
-% grad_out.rb = - 2 * Qa2 * B * Qa2';
-
+%
+grad_out = Qcd_i * Ri * A' * P + B;
 end 
 
+%%
 function c_out = mycost_rb(qc, x, node_deg, Qa, Qcd_i, Ri)
 Qcdd_i = qc;
 rb_i = x;
@@ -145,36 +116,22 @@ d = size(Ri,2);
 P = [zeros(p-d,d), eye(p-d)];
 A = P * Qcdd_i' * Qcd_i * Ri;
 % B
-%     Qb_i = eye(p);
-%     Qb_i(p-1:end, p-1:end) = rb_i;
 Qb_i = blkdiag(eye(p-node_deg), rb_i); %node_deg = 2
 B = Qcdd_i' - Qa' * Qb_i * Qa;
 
 % sum of norms
-c_out = norm(A(:))^2 + norm(B(:))^2;
+c_out = 0.5 * (norm(A(:))^2 + norm(B(:))^2);
 end
 
-function grad_out = myegrad_rb(qc, x, node_deg, ...
-    Qa, Ri)
+function grad_out = myegrad_rb(qc, x, node_deg, Qa, Ri)
 Qcdd_i = qc;
 rb_i = x;
 p = size(Ri,1);
-% d = size(Ri,2);
-% A
-% P = [zeros(p-d,d), eye(p-d)];
-% Qcd_i = eye(p);
-% A = P * Qcdd_i' * Qcd_i * Ri;
-% B
-%     Qb_i = eye(p);
-%     Qb_i(p-1:end, p-1:end) = rb_i;
+
 Qb_i = blkdiag(eye(p-node_deg), rb_i); %node_deg = 2
 B = Qcdd_i' - Qa' * Qb_i * Qa;
-% grad out (struct)
-% Qa1 = Qa(1:2, :);
+
 Qa2 = Qa(3:end, :);
-% grad_out.qc = 2 * Qcd_i * Ri * A' * P + B;
-%     grad_out.rb = - Q2 * B * Q2';
-grad_out = - 2 * Qa2 * B * Qa2';
 
-
+grad_out = - Qa2 * B * Qa2';
 end 
