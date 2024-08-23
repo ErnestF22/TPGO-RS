6cab922165db32ba5a018e78c50eaa67a8bd635b
ErnestF22 <ernesto.fontana96@gmail.com>
2024-08-23 16:49:08 +0300

    Added global rot+transl check

--- genproc/check_recovery_error.m
+++ genproc/check_recovery_error.m
@@ -105,18 +105,46 @@ cost_out = rsom_cost_base(X_recovered, problem_struct_next);
 disp("cost_out")
 disp(cost_out)
 
-transf_out = RT2G(R_recovered, T_recovered); %??
+% transf_out = RT2G(R_recovered, T_recovered); %rsom_genproc() function output
 
 disp("[matStackH(X_gt.R); matStackH(R_recovered)]");
 disp([matStackH(X_gt.R); matStackH(R_recovered)]);
 
 R_global = R_recovered(:,:,1) * X_gt.R(:,:,1)'; %!!
+% code for making all rotations global at once
+R_recovered_global = multiprod(repmat(R_global', 1, 1, N), R_recovered);
+disp("[matStackH(X_gt.R); matStackH(R_recovered_global)]");
+disp([matStackH(X_gt.R); matStackH(R_recovered_global)]);
+
+T_global = R_global * T_recovered(:,1) - X_gt.T(:,1); %!!
+% code for making all translation global at once
+disp("[X_gt.T; T_recovered]");
+T_recovered_global = R_global' * T_recovered - T_global;
+disp([X_gt.T; T_recovered_global]);
 
 for ii = 1:N
     R_gt_i = X_gt.R(:,:,ii);
-    R_recov_i = (R_recovered(:,:,ii)' * R_global)';
+    R_recov_i_global = R_recovered_global(:,:,ii); %GLOBAL!
+    fprintf("ii %g\n", ii);
+    % rotations
     disp("R_gt_i, R_recov_i");
-    disp([R_gt_i, R_recov_i]);
+    disp([R_gt_i, R_recov_i_global]);
+    disp("is_equal_floats(R_gt_i, R_recov_i_global)")
+    disp(is_equal_floats(R_gt_i, R_recov_i_global))
+    if (~is_equal_floats(R_gt_i, R_recov_i_global))
+        error("rot found NOT equal")
+    end
+    % translations
+    T_gt_i = X_gt.T(:,ii);
+    T_recov_i_global = T_recovered_global(:,ii);
+    disp("[X_gt.T, T_recovered]");
+    disp([T_gt_i, T_recov_i_global]);
+    disp("is_equal_floats(T_gt_i, T_recov_i_global)")
+    disp(is_equal_floats(T_gt_i, T_recov_i_global))
+    if (~is_equal_floats(T_gt_i, T_recov_i_global))
+        error("transl found NOT equal")
+
+    end
 end
 
 end %file function
