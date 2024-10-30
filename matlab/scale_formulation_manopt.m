b62b3877436966a54649aa907d71fe44d65f3449
ErnestF22 <ernesto.fontana96@gmail.com>
2024-10-30 17:34:57 +0100

    Fixed case with rho = 0

--- matlab/scale_formulation_manopt.m
+++ matlab/scale_formulation_manopt.m
@@ -26,19 +26,25 @@ tuple.T = euclideanfactory(nrs, N);
 tuple.lambda = euclideanfactory(num_edges, 1);
 M = productmanifold(tuple);
 
-problem_data.rho = 1.0;
+problem_data.rho = 0.0;
 
 problem.M = M;
 problem_data.sz = [nrs, d, N];
 problem.cost = @(x) ssom_cost(x, problem_data);
-% problem.egrad = @(x) ssom_egrad(x, problem_data);
-problem.grad = @(x) ssom_rgrad(x, problem_data);
+problem.egrad = @(x) ssom_egrad(x, problem_data);
+% problem.grad = @(x) ssom_rgrad(x, problem_data);
 % problem.hess = @(x, u) hess_genproc(x, u, problem_data);
 
+fprintf("\n");
+disp("Gradient check easy");
 X_gradcheck.R = eye3d(nrs, d, N);
 X_gradcheck.T = zeros(nrs, N);
-X_gradcheck.lambda = zeros(num_edges, 1);
+X_gradcheck.lambda = ones(num_edges, 1);
 checkgradient(problem, X_gradcheck);
+
+fprintf("\n");
+disp("Gradient check rand");
+checkgradient(problem);
 % checkhessian(problem);
 
 % problem_data.R_gt = eye3d(nrs, d, N);
