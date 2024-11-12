8757c3dbc44a1b1d4268c4d94f6b798f74f61a43
ErnestF22 <ernesto.fontana96@gmail.com>
2024-11-12 18:00:39 +0100

    Added rsomPimHessianGenproc

--- include/thirdparty/roptlib/problems_SampleSom.h
+++ include/thirdparty/roptlib/problems_SampleSom.h
@@ -232,6 +232,126 @@ namespace ROPTLIB
          * Full size of problem: pxdxn (rotations) + pxn (translations)
          */
         int fullSz_;
+
+        void rsomPimHessianGenproc(double thresh) const
+        {
+            // [Y_star, lambda, v] = rsom_pim_hessian_genproc( ...
+            //     X, problem_struct_next, thr);
+            // disp("v") // %just to remove unused variable warning
+            // disp(v)
+            // if lambda > 0
+            //     disp("R, T eigenvals > 0: exiting staircase")
+            // break;
+
+            /////////////////////////////////////////////////////
+            // if ~exist('thresh', 'var')
+            //     thresh = 1e-6;
+            // end
+
+            // Rnext = cat_zero_rows_3d_array(X.R);
+            // Tnext = cat_zero_row(X.T);
+            // Xnext.R = Rnext;
+            // Xnext.T = Tnext;
+            // rhess_fun_han = @(u) hess_genproc(Xnext,u,problem_struct_next);
+
+            // stiefel_normalize_han = @(x) x./ (norm(x(:))); //Note: this is basically eucl_normalize_han
+
+            // u_start.R = stiefel_randTangentNormVector(Rnext);
+            // u_start.R = stiefel_normalize(Rnext, u_start.R);
+            // u_start.T = rand(size(Tnext));
+            // u_start.T = stiefel_normalize_han(u_start.T);
+            // [lambda_pim, v_pim] = pim_function_genproc(rhess_fun_han, u_start, stiefel_normalize_han, thresh);
+            // disp('Difference between lambda*v_max and H(v_max) should be in the order of the tolerance:')
+            // eigencheck_hessian_genproc(lambda_pim, v_pim, rhess_fun_han);
+
+
+
+            // if lambda_pim>0
+            // // %     fprintf("lambda_pim R %g\n", lambda_pim.R);
+            // // %     fprintf("lambda_pim T %g\n", lambda_pim.T);
+            //     fprintf("lambda_pim %g\n", lambda_pim);
+                
+            // // %     lambda_pim = max(lambda_pim.R, lambda_pim.T);
+
+            //     mu = 1.1 * lambda_pim;
+
+            //     rhess_shifted_fun_han = ...
+            //         @(u) hess_genproc_shifted(Xnext,u,mu,problem_struct_next);
+                        
+            //     // %run shifted power iteration
+            //     u_start_second_iter.R = stiefel_randTangentNormVector(Rnext);
+            //     u_start_second_iter.R = stiefel_normalize(Rnext, u_start_second_iter.R);
+            //     u_start_second_iter.T = rand(size(Tnext));
+            //     u_start_second_iter.T = stiefel_normalize_han(u_start.T);
+            //     [lambda_pim_after_shift, v_pim_after_shift] = pim_function_genproc( ...
+            //         rhess_shifted_fun_han, u_start_second_iter, stiefel_normalize_han, thresh);
+                
+            //     disp(['Difference between lambda_pim_after_shift*v_pim_after_shift ' ...
+            //         'and H_SH(v_pim_after_shift) should be in the order of the tolerance:'])
+            //     eigencheck_hessian_genproc(lambda_pim_after_shift, v_pim_after_shift, ...
+            //         rhess_shifted_fun_han);
+
+
+            //     highest_norm_eigenval = lambda_pim_after_shift + mu;
+
+
+
+            // // %Preparing linesearch
+            // nrs_next = problem_struct_next.sz(1);
+            // d = problem_struct_next.sz(2);
+            // N = problem_struct_next.sz(3);
+            // tuple_next.R = stiefelfactory(nrs_next, d, N);
+            // tuple_next.T = euclideanfactory(nrs_next, N);
+            // M = productmanifold(tuple_next);
+            // step2.M = M;
+            // step2.sz = [nrs_next, d, N];
+            // step2.cost = @(x) cost_genproc(x, problem_struct_next);
+            // step2.grad = @(x) grad_genproc(x, problem_struct_next);
+            // step2.hess = @(x, u) hess_genproc(x, u, problem_struct_next);
+
+
+
+            // // % alpha = min(lambdas_moved) + lambdas_max;
+            // // % alpha_linesearch = 10; // %TODO: set this correctly
+            // // % SDPLRval = 10; // %TODO: set this correctly 
+
+            // disp("Now performing linesearch...");
+            // // %Note: first output param of linesearch() would be "stepsize"
+            // [~, Y0] = linesearch_decrease(step2, ...
+            //     Xnext, v_pim_after_shift, cost_genproc(Xnext,problem_struct_next));
+
+            // lambda_pim_out = highest_norm_eigenval;
+            // v_pim_out = v_pim_after_shift;
+
+
+        }
     };
 
 } // end of namespace ROPTLIB
+
+// "CODE FOR PLOTTING CONCAVITY"
+// alphas = linspace(-0.01,0.01,501); %-0.2:0.01:0.2;
+// plot_vals = zeros(size(alphas));
+// plot_vals_taylor = zeros(size(alphas));
+// for ii = 1:length(alphas)
+//     x_retr_ii = step2.M.retr(Xnext, v_pim_after_shift, alphas(ii));
+// %     disp("Is x_retr_ii on Stiefel? (Taylor)")
+// %     disp(check_is_on_stiefel(x_retr_ii));
+// %     disp([matStack(x), matStack(x_retr_ii)])
+//     plot_vals(ii) = step2.cost(x_retr_ii);
+//     %Note: gradient is zero
+//     pvt_R = alphas(ii)^2/2* ...
+//         sum(stiefel_metric( ...
+//         Rnext,v_pim_after_shift.R, ...
+//             hess_genproc_R(Xnext, v_pim_after_shift, problem_struct_next),'canonical'));
+//     pvt_T = alphas(ii)^2/2* ...
+//         sum(stiefel_metric( ...
+//         Tnext,v_pim_after_shift.T, ...
+//             hess_genproc_T(Xnext, v_pim_after_shift, problem_struct_next),'euclidean'));
+//     plot_vals_taylor(ii) = step2.cost(Xnext) + pvt_R + pvt_T;
+// end
+
+// plot(alphas, plot_vals,'b')
+// hold on
+// plot(alphas,plot_vals_taylor,'k.');
+// hold off
