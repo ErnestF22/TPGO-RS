7c13d348e058766f8fbf4e263a4fa775c91b639f
ErnestF22 <ernesto.fontana96@gmail.com>
2024-11-28 15:37:41 +0100

    Added grad_local

--- matlab/check_ssom_curve/test_check_ssom_test_ehess_lambda_R.m
+++ matlab/check_ssom_curve/test_check_ssom_test_ehess_lambda_R.m
@@ -13,7 +13,7 @@ vLambda0=rand(e,1,1);
 
 R0=randrot(d,N);
 vR0=rot_randTangentNormVector(R0);
-[R,dR,~,~,~]=rot_geodFun(R0, vR0);
+[R,dR,~,~,~,ddR]=rot_geodFun(R0, vR0);
 
 T0 = rand(d,N);
 % vT0 = rand(d,N);
@@ -26,12 +26,44 @@ curve.dc=@(t) dLambda(t);
 curve.ddc=@(t) ddLambda(t);
 
 % f=@(t) problem.cost(curve.c(t));
-gradf=@(t) problem.egrad_lambda(lambda0);
-df=@(t) sum(stiefel_metric([],gradf(t),vLambda0));
+
+grad_handle = @(x) grad_lambda_local(lambda0, x, T0, problem);
+gradf=@(t) grad_handle(R(t));
+df=@(t) sum(stiefel_metric([],gradf(t),lambda0));
 % funCheckDer(f,df)
 ehessf = @(t) problem.ssom_ehess_lambda_r(lambda0,vLambda0,T0,R(t),dR(t));
 ddf_1 = @(t) stiefel_metric([], ehessf(t), curve.dc(t), 'euclidean');
 ddf_2 = @(t) stiefel_metric([], gradf(t), curve.ddc(t), 'euclidean');
 ddf = @(t) sum(ddf_1(t) + ddf_2(t));    
 
-funCheckDer(df,ddf)
+funCheckDer(df,ddf,'angle')
+end
+
+function g=grad_lambda_local(lambda, R, T, problem_data)
+    edges = problem_data.edges;
+    tijs_vec = problem_data.tijs;
+    rho = problem_data.rho;
+    
+    % x = lambdas in this context
+    
+    g = zeros(length(lambda), 1);
+    
+    num_edges = size(edges, 1);
+    for ee = 1:num_edges
+        ii = edges(ee, 1);
+        jj = edges(ee, 2);
+        lambda_e = lambda(ee);
+        tij_e = tijs_vec(:, ee);
+        T_i = T(:, ii);
+        T_j = T(:, jj);
+        R_i = R(:, :, ii);
+        a = T_i - T_j;
+        b = R_i * tij_e;
+        base_part = 2*(b' * b * lambda_e + a' * b);
+        relu_part = 0.0;
+        if (ssom_relu_argument(lambda_e)>0)
+            relu_part = ssom_relu_argument(lambda_e);
+        end
+        g(ee) = base_part + rho * relu_part;
+    end
+end
