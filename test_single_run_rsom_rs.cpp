033cd5e3493430cdcbba48696b7dcf02bd201edc
ernestf22 <ernesto.fontana@unipr.it>
2024-12-16 11:56:49 +0100

    Added RSOM RS (up until recovery excluded)

--- test_single_run_rsom_rs.cpp
+++ test_single_run_rsom_rs.cpp
@@ -10,9 +10,118 @@
 
 #include "thirdparty/roptlib/problems_SampleSom.h"
 
+void runRsomRS(ROPTLIB::SampleSomProblem &Prob, const ROPTLIB::Vector &startX)
+{
+    // output the parameters of the manifold of domain
+    ROPTLIB::RTRNewton *RTRNewtonSolver = new ROPTLIB::RTRNewton(&Prob, &startX); // USE INITGUESS HERE!
+    RTRNewtonSolver->Verbose = ROPTLIB::ITERRESULT;
+    // RTRNewtonSolver->Max_Iteration = 500;
+    // RTRNewtonSolver->Max_Inner_Iter = 500;
+    // ROPTLIB::PARAMSMAP solverParams = {std::pair<std::string, double>("Max_Inner_Iter", 10)};
+    // RTRNewtonSolver->SetParams(solverParams);
+    RTRNewtonSolver->CheckParams();
+
+    // % Solve.
+    // [x, xcost, info, options] = trustregions(problem);
+    RTRNewtonSolver->Run();
+    // Numerically check gradient consistency (optional).
+    auto Xopt = RTRNewtonSolver->GetXopt();
+    auto XoptCost = RTRNewtonSolver->Getfinalfun();
+
+    Prob.CheckGradHessian(Xopt);
+
+    // std::cout << "Prob.GetUseGrad() " << Prob.GetUseGrad() << std::endl;
+    // std::cout << "Prob.GetUseHess() " << Prob.GetUseHess() << std::endl;
+    // std::cout << "Prob.GetNumGradHess() " << Prob.GetNumGradHess() << std::endl;
+
+    // Outputs
+    Xopt.Print("Xopt");
+    std::cout << "XoptCost " << XoptCost << std::endl; // x cost
+
+    delete RTRNewtonSolver;
+
+    // RS
+
+    int d = Prob.sz_.d_;
+    int n = Prob.sz_.n_;
+    int r0 = d + 1;
+
+    integer numoftypes = 2; // 2 i.e. (3D) Stiefel + Euclidean
+    integer numofmani1 = n; // num of Stiefel manifolds
+    integer numofmani2 = 1;
+
+    SomUtils::MatD XoptEigVec(SomUtils::MatD::Zero(d * d * n + d * n, 1));
+    Prob.RoptToEig(Xopt, XoptEigVec);
+    ROFL_VAR1(XoptEigVec.transpose())
+
+    for (int staircaseStepIdx = r0; staircaseStepIdx <= d * n + 1; ++staircaseStepIdx)
+    {
+        ROFL_VAR1(staircaseStepIdx)
+
+        SomUtils::VecMatD R(n, SomUtils::MatD::Zero(staircaseStepIdx - 1, d));
+        SomUtils::MatD T(SomUtils::MatD::Zero(staircaseStepIdx - 1, n));
+        Prob.getRotations(XoptEigVec, R);
+        Prob.getTranslations(XoptEigVec, T);
+
+        SomUtils::VecMatD Rnext(n, SomUtils::MatD::Zero(staircaseStepIdx, d));
+        SomUtils::MatD Tnext(SomUtils::MatD::Zero(staircaseStepIdx, n));
+
+        Prob.catZeroRow3dArray(R, Rnext);
+        Prob.catZeroRow(T, Tnext);
+
+        auto somSzNext = Prob.sz_;
+        somSzNext.p_++;
+
+        ROPTLIB::SampleSomProblem ProbNext(somSzNext, Prob.Tijs_, Prob.edges_);
+
+        ROPTLIB::Stiefel mani1next(somSzNext.p_, somSzNext.d_);
+        mani1next.ChooseParamsSet2();
+        ROPTLIB::Euclidean mani2next(somSzNext.p_, somSzNext.n_);
+        ROPTLIB::ProductManifold ProdManiNext(numoftypes, &mani1next, numofmani1, &mani2next, numofmani2);
+        ROPTLIB::Vector Y0 = ProdManiNext.RandominManifold();
+        ProbNext.rsomPimHessianGenproc(1e-4, Rnext, Tnext, Y0);
+
+        // Run next step of staircase with found initial guess
+
+        ROPTLIB::RTRNewton *RTRNewtonSolverNext = new ROPTLIB::RTRNewton(&ProbNext, &Y0); // USE INITGUESS HERE!
+        RTRNewtonSolverNext->Verbose = ROPTLIB::ITERRESULT;
+        // RTRNewtonSolverNext->Max_Iteration = 500;
+        // RTRNewtonSolverNext->Max_Inner_Iter = 500;
+        // ROPTLIB::PARAMSMAP solverParams = {std::pair<std::string, double>("Max_Inner_Iter", 10)};
+        // RTRNewtonSolverNext->SetParams(solverParams);
+        RTRNewtonSolverNext->CheckParams();
+
+        // % Solve.
+        // [x, xcost, info, options] = trustregions(problem);
+        RTRNewtonSolverNext->Run();
+        // Numerically check gradient consistency (optional).
+        auto XoptNext = RTRNewtonSolverNext->GetXopt();
+        auto XoptNextCost = RTRNewtonSolverNext->Getfinalfun();
+
+        ProbNext.CheckGradHessian(XoptNext);
+
+        // std::cout << "Prob.GetUseGrad() " << Prob.GetUseGrad() << std::endl;
+        // std::cout << "Prob.GetUseHess() " << Prob.GetUseHess() << std::endl;
+        // std::cout << "Prob.GetNumGradHess() " << Prob.GetNumGradHess() << std::endl;
+
+        // Outputs
+        XoptNext.Print("XoptNext");
+        std::cout << "XoptNextCost " << XoptNextCost << std::endl; // x cost
+
+        XoptEigVec.resize(staircaseStepIdx * d * n + staircaseStepIdx * n, 1);
+        Prob.RoptToEig(XoptNext, XoptEigVec);
+        ROFL_VAR1(XoptEigVec.transpose())
+
+        delete RTRNewtonSolverNext;
+
+        break;
+    }
+
+    // Implement recovery procedure
+}
+
 int main(int argc, char **argv)
 {
-
     int d = 3;
     int n;
     SomUtils::readSingleIntCsv("../matlab/data/cpp_testdata/tdata_n10_mindeg3/n.csv", n);
@@ -21,7 +130,7 @@ int main(int argc, char **argv)
 
     SomUtils::readSingleIntCsv("../matlab/data/cpp_testdata/tdata_n10_mindeg3/e.csv", numEdges);
 
-    ROFL_VAR2(n, numEdges); //n = 10, numEdges = 48
+    ROFL_VAR2(n, numEdges); // n = 10, numEdges = 48
 
     SomUtils::SomSize somSzD(d, d, n);
     SomUtils::MatD Tijs(d, numEdges);
@@ -46,7 +155,12 @@ int main(int argc, char **argv)
     SomUtils::readCsvInitguess("../matlab/data/cpp_testdata/tdata_n10_mindeg3/X_gt.csv", xGt);
     xGt.Print("xGt");
     ROPTLIB::SampleSomProblem Prob(somSzD, Tijs, edges);
+    // Set the domain of the problem to be the product of Stiefel manifolds
     Prob.SetDomain(&ProdMani);
+
+    // Set Prob params
+    Prob.SetUseGrad(true);
+    Prob.SetUseHess(true);
 
     // ROPT to Eig (GT)
     SomUtils::MatD XgtVecEig(SomUtils::MatD::Zero(d * d * n + d * n, 1));
@@ -71,4 +185,10 @@ int main(int argc, char **argv)
     // ROPTLIB::Vector xManoptOut = ProdManiNrs.RandominManifold();
     // SomUtils::readCsvInitguess("../data/recov_x_manopt_out.csv", xManoptOut);
     // xManoptOut.Print("xManoptOut");
+
+    // Generate startX (random)
+    ROPTLIB::Vector startX = ProdMani.RandominManifold();
+
+    // RUN RSOM RS
+    runRsomRS(Prob, startX);
 }
