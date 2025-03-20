d7683d01c99436aa544e4bb15952de0b3eb84872
ErnestF22 <ernesto.fontana@unipr.it>
2025-03-20 12:14:18 +0100

    Added plot test noisy results, eigvalue for rsom escape

--- plot_rsom_cpp_test_noisy_results.py
+++ plot_rsom_cpp_test_noisy_results.py
@@ -1,65 +1,80 @@
+import os
+
 import numpy as np
 
-folder = "results/n5_mindeg2_sigma00_20250205_1707_59/"
-
-testid = folder[8: 27]
-
-print("testid")
-print(testid)
 
 
-testid_split = testid.split("_")
-n = int(testid_split[0][1:])
-mindeg = int(testid_split[1][6:])
-sigma = float(testid_split[2][5:])
-print("n")
-print(n)
-print("mindeg")
-print(mindeg)
-print("sigma")
-print(sigma)
+rsom_rs_results_path = "results/"
+folders = os.listdir(rsom_rs_results_path)
 
-#rot errs
-rot_errs = np.array([], dtype=np.float64)
-file_rot_errs = folder + testid + "rot_errors_mean.txt"
-print("file_rot_errs")
-print(file_rot_errs)
-with open(file_rot_errs, 'r') as f:
-    for count, line in enumerate(f, start=1):
-        if count % 2 == 0:
-            # print(line)
-            rot_errs = np.append(rot_errs, float(line))
+for f in folders:
 
-rot_errs_mean = np.average(rot_errs)
-print("rot_errs_mean")
-print(rot_errs_mean)
+    print("f")
+    print(f)
 
-#transl errs
-transl_errs = np.array([], dtype=np.float64)
-file_transl_errs = folder + testid + "transl_errors_mean.txt"
-print("file_transl_errs")
-print(file_transl_errs)
-with open(file_transl_errs, 'r') as f:
-    for count, line in enumerate(f, start=1):
-        if count % 2 == 0:
-            # print(line)
-            transl_errs = np.append(transl_errs, float(line))
+    folder = rsom_rs_results_path + f
 
-transl_errs_mean = np.average(transl_errs)
-print("transl_errs_mean")
-print(transl_errs_mean)
+    print("folder")
+    print(folder)
 
-#exec time
-exec_times = np.array([], dtype=np.float64)
-file_exec_times = folder + testid + "exec_times.txt"
-print("file_exec_times")
-print(file_exec_times)
-with open(file_exec_times, 'r') as f:
-    for count, line in enumerate(f, start=1):
-        if count % 2 == 0:
-            # print(line)
-            exec_times = np.append(exec_times, float(line))
+    testid = folder[8: 27]
 
-exec_times_mean = np.average(exec_times)
-print("exec_times_mean")
-print(exec_times_mean)
+    print("testid")
+    print(testid)
+
+
+    testid_split = testid.split("_")
+    n = int(testid_split[0][1:])
+    mindeg = int(testid_split[1][6:])
+    sigma = float(testid_split[2][5:])
+    print("n")
+    print(n)
+    print("mindeg")
+    print(mindeg)
+    print("sigma")
+    print(sigma)
+
+    #rot errs
+    rot_errs = np.array([], dtype=np.float64)
+    file_rot_errs = folder + "/" + testid + "rot_errors_mean.txt"
+    print("file_rot_errs")
+    print(file_rot_errs)
+    with open(file_rot_errs, 'r') as f:
+        for count, line in enumerate(f, start=1):
+            if count % 2 == 0:
+                # print(line)
+                rot_errs = np.append(rot_errs, float(line))
+
+    rot_errs_mean = np.average(rot_errs)
+    print("rot_errs_mean")
+    print(rot_errs_mean)
+
+    #transl errs
+    transl_errs = np.array([], dtype=np.float64)
+    file_transl_errs = folder + "/" + testid + "transl_errors_mean.txt"
+    print("file_transl_errs")
+    print(file_transl_errs)
+    with open(file_transl_errs, 'r') as f:
+        for count, line in enumerate(f, start=1):
+            if count % 2 == 0:
+                # print(line)
+                transl_errs = np.append(transl_errs, float(line))
+
+    transl_errs_mean = np.average(transl_errs)
+    print("transl_errs_mean")
+    print(transl_errs_mean)
+
+    #exec time
+    exec_times = np.array([], dtype=np.float64)
+    file_exec_times = folder + "/" + testid + "exec_times.txt"
+    print("file_exec_times")
+    print(file_exec_times)
+    with open(file_exec_times, 'r') as f:
+        for count, line in enumerate(f, start=1):
+            if count % 2 == 0:
+                # print(line)
+                exec_times = np.append(exec_times, float(line))
+
+    exec_times_mean = np.average(exec_times)
+    print("exec_times_mean")
+    print(exec_times_mean)
